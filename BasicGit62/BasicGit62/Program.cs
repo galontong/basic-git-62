643b66edb4a354c972741af2210bc50a2e07721a
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2022-01-17 09:28:55 +0700

    Create asp.net core project

---
+++ BasicGit62/BasicGit62/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicGit62
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
